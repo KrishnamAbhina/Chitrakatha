17a7b6c73455ba068c9a58d858ab04b819322ec2
Krishnam Abhina <134093012+KrishnamAbhina@users.noreply.github.com>
2023-08-15 21:41:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+
+
+const button = document.getElementById("FAQButton");
+button.addEventListener("click", function() {
+  window.location.href = "faq.html";
+});
